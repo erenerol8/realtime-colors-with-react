7fc295885ae54418573a1331e6815152eb6ea871
Eren <erenerol8@gmail.com>
2022-10-11 13:34:28 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Palette from "./components/Palette";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Palette />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Palette.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Palette = () => {
+  return (
+    <div className="palette">
+      <input type="color"></input>
+      <button>click</button>
+    </div>
+  );
+};
+
+export default Palette;
