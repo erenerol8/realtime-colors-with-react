be85b5bc2dd04657a4c7417b66be425a9dbbe2e2
Eren <erenerol8@gmail.com>
2022-10-11 14:13:59 +0300

    subscribe

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
 import "./App.css";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Palette from "./components/Palette";
-import { init } from "./socketApi";
+import { init, subscribe } from "./socketApi";
 
 function App() {
+  const [activeColor, setActiveColor] = useState("#282c34");
+
   useEffect(() => {
     init();
+    subscribe((color) => {
+      setActiveColor(color);
+    });
   }, []);
 
   return (
-    <div className="App">
-      <Palette />
+    <div className="App" style={{ backgroundColor: activeColor }}>
+      <h1>{activeColor}</h1>
+      <Palette activeColor={activeColor} />
     </div>
   );
 }
--- client/src/components/Palette.js
+++ client/src/components/Palette.js
@@ -1,17 +1,16 @@
 import { useState } from "react";
 import { send } from "../socketApi";
 
-const Palette = () => {
+const Palette = ({ activeColor }) => {
   const [color, setColor] = useState("color");
   return (
     <div className="palette">
       <input
         type="color"
-        value={color}
+        value={activeColor}
         onChange={(e) => setColor(e.target.value)}
       />
       <button onClick={() => send(color)}>click</button>
-      {color}
     </div>
   );
 };
